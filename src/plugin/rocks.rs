bbcc0f7db2547b7f2c9685ab010426e71bf76400
jungyongum <ian.jungyong.um@gmail.com>
2021-07-30 19:25:18 +0900

    divide chain rocks db and default rocks db

--- src/plugin/rocks.rs
+++ src/plugin/rocks.rs
@@ -1,17 +1,16 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use appbase::*;
-use jsonrpc_core::{Params, serde_from_str};
+use jsonrpc_core::Params;
 use rocksdb::{DB, DBWithThreadMode, SingleThreaded};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Map, Value};
 
 use crate::{enumeration, message};
 use crate::libs::serde::get_str;
-use crate::libs::serialize;
 use crate::plugin::jsonrpc::JsonRpcPlugin;
 use crate::types::enumeration::Enumeration;
-use crate::validation::{find_by_key, get_task};
+use crate::validation::find_by_key;
 
 pub struct RocksPlugin {
     base: PluginBase,
@@ -21,30 +20,11 @@ pub struct RocksPlugin {
 
 message!((RocksMsg; {key: String}, {value: Value}); (RocksMethod; {Put: "put"}, {Delete: "delete"}));
 
-type RocksDB = Arc<Mutex<DBWithThreadMode<SingleThreaded>>>;
+type RocksDB = Arc<DBWithThreadMode<SingleThreaded>>;
 
 impl RocksPlugin {
-    pub fn find_by_prefix(&self, prefix: &str) -> Value {
-        let db = Arc::clone(self.db.as_ref().unwrap());
-        Self::find_by_prefix_static(&db, prefix)
-    }
-
-    fn find_by_prefix_static(db: &RocksDB, prefix: &str) -> Value {
-        let db_lock = db.lock().unwrap();
-        let mut iter = db_lock.raw_iterator();
-        iter.seek(prefix.as_bytes());
-        let mut result: Vec<Value> = Vec::new();
-        while iter.valid() && serialize::deserialize(iter.key().unwrap()).starts_with(prefix) {
-            let value: Map<String, Value> = serde_from_str(serialize::deserialize(iter.value().unwrap()).as_str()).unwrap();
-            result.push(Value::Object(value));
-            iter.next();
-        }
-        Value::Array(result)
-    }
-
-    pub fn find_by_key(db: &RocksDB, key: &str) -> Value {
-        let db_lock = db.lock().unwrap();
-        let result = db_lock.get(key.as_bytes()).unwrap();
+    pub fn find_by_prefix_static(db: &RocksDB, key: &str) -> Value {
+        let result = db.get(key.as_bytes()).unwrap();
         match result {
             None => {
                 Value::Null
@@ -74,31 +54,12 @@ impl Plugin for RocksPlugin {
             return;
         }
 
-        self.db = Some(Arc::new(Mutex::new(DB::open_default("rocks").unwrap())));
+        self.db = Some(Arc::new(DB::open_default("rocks").unwrap()));
         self.monitor = Some(app::subscribe_channel(String::from("rocks")));
 
         let jsonrpc_plugin_handle = app::get_plugin::<JsonRpcPlugin>();
         let mut jsonrpc_plugin = jsonrpc_plugin_handle.lock().unwrap();
         let jsonrpc = jsonrpc_plugin.downcast_mut::<JsonRpcPlugin>().unwrap();
-
-        let db = Arc::clone(self.db.as_ref().unwrap());
-        jsonrpc.add_method(String::from("get_tasks"), move |params: Params| {
-            let params: Map<String, Value> = params.parse().unwrap();
-            let verified = get_task::verify(&params);
-            if verified.is_err() {
-                return Box::new(futures::future::ready(Ok(Value::String(verified.unwrap_err()))));
-            }
-            let prefix = match params.get("task_id") {
-                None => {
-                    "task"
-                }
-                Some(task_id) => {
-                    task_id.as_str().unwrap()
-                }
-            };
-            let tasks = Self::find_by_prefix_static(&db, prefix);
-            Box::new(futures::future::ready(Ok(tasks)))
-        });
 
         let db = Arc::clone(self.db.as_ref().unwrap());
         jsonrpc.add_method(String::from("find_by_key"), move |params: Params| {
@@ -108,7 +69,7 @@ impl Plugin for RocksPlugin {
                 return Box::new(futures::future::ready(Ok(Value::String(verified.unwrap_err()))));
             }
             let key = get_str(&params, "key").unwrap();
-            let value = Self::find_by_key(&db, key);
+            let value = Self::find_by_prefix_static(&db, key);
             Box::new(futures::future::ready(Ok(value)))
         });
     }
@@ -122,7 +83,6 @@ impl Plugin for RocksPlugin {
         tokio::spawn(async move {
             let mut mon_lock = monitor.lock().await;
             loop {
-                let db_lock = db.lock().unwrap();
                 if let Ok(msg) = mon_lock.try_recv() {
                     let data = msg.as_object().unwrap();
                     let method = RocksMethod::find(data.get("method").unwrap().as_str().unwrap()).unwrap();
@@ -130,11 +90,11 @@ impl Plugin for RocksPlugin {
                         RocksMethod::Put => {
                             let key = get_str(data, "key").unwrap();
                             let val = get_str(data, "value").unwrap();
-                            let _ = db_lock.put(key.as_bytes(), val.as_bytes());
+                            let _ = db.put(key.as_bytes(), val.as_bytes());
                         }
                         RocksMethod::Delete => {
                             let key = get_str(data, "key").unwrap();
-                            let _ = db_lock.delete(key.as_bytes());
+                            let _ = db.delete(key.as_bytes());
                         }
                     }
                 }
